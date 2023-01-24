413474ca5039b73edb53ab3a1a065af3e2852250
Sadnan-2019 <sadnan.wow@gmail.com>
2023-01-24 17:03:37 +0600

    services responcive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Aside from './Components/Aside/Aside';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="">
       <Navbar></Navbar>
       <Aside></Aside>
+      <Footer></Footer>
       
     </div>
   );
