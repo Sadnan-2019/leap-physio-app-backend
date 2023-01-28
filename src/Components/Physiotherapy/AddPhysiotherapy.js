ef5670899c419592280eca6f39f6cad2a7633817
Sadnan-2019 <sadnan.wow@gmail.com>
2023-01-28 14:46:36 +0600

    services responcive

--- src/Components/Physiotherapy/AddPhysiotherapy.js
+++ src/Components/Physiotherapy/AddPhysiotherapy.js
@@ -4,16 +4,18 @@ const AddPhysiotherapy = () => {
   return (
     <div className="container">
       <div className="row justify-content-center align-items-center ">
-        <div className="col-lg-6   d-flex   align-items-center gap-3    ">
+        <div className="col-lg-6   d-flex   align-items-center gap-3 mt-5   ">
           <div class="col-lg-6   ">
             <div class="form-group p-2">
             <div class=" ">
                     <div
-                      class="card info-card   border border-dark  rounded-5"
+                      class="d-flex flex-column    border border-dark rounded   "
                       style={{ height: "280px" }}
                     >
-                      <div class="mx-auto      ">
-                        <h5 class="card-title mt-5">Upload Passport Size Image</h5>
+                      <div class="    text-center  mx-auto   ">
+                        <h5 class="  mt-5">Upload Passport Size Image</h5>
+                        
+
                         <button type="submit" class="btn btn-primary">Choose Picture</button>
                       </div>
                     </div>
@@ -64,6 +66,47 @@ const AddPhysiotherapy = () => {
           
         </div>
         
+      </div>
+
+      <div className="row d-flex justify-content-center align-items-center">
+      <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
+            <div class="form-group p-2 col-lg-6 "  >
+              <label>Select Division</label>
+              <select class="form-control select2" style={{ width: "100%" }}>
+                <option selected="selected">Alabama</option>
+                <option>Alaska</option>
+                {/* <option disabled="disabled">California (disabled)</option> */}
+                <option>Delaware</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Washington</option>
+              </select>
+            </div>
+            <div class="form-group p-2 col-lg-6" >
+              <label>Select District</label>
+              <select class="form-control select2" style={{ width: "100%" }}>
+                <option selected="selected">Alabama</option>
+                <option>Alaska</option>
+                {/* <option disabled="disabled">California (disabled)</option> */}
+                <option>Delaware</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Washington</option>
+              </select>
+            </div>
+            <div class="form-group p-2 col-lg-6">
+              <label>Select Thana </label>
+              <select class="form-control select2" style={{ width: "100%" }}>
+                <option selected="selected">Alabama</option>
+                <option>Alaska</option>
+                {/* <option disabled="disabled">California (disabled)</option> */}
+                <option>Delaware</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Washington</option>
+              </select>
+            </div>
+          </div>
       </div>
     </div>
   );
