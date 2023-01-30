77a5bccff7df0c9cc38e24bd4f18efbd32265085
Sadnan-2019 <sadnan.wow@gmail.com>
2023-01-30 16:22:00 +0600

    services responcive

--- src/Components/Patients/AddPatients.js
+++ src/Components/Patients/AddPatients.js
@@ -1,90 +1,96 @@
- import React from 'react';
- 
- const AddPatients = () => {
-    return (
-        <div>
-              <div className="container-fluid ">
-      <div className="row justify-content-center align-items-center ">
-        <div className="col-lg-6   d-flex   align-items-center gap-3 mt-5   ">
-          <div class="col-lg-6   ">
-            <div class="form-group p-2">
-            <div class=" ">
-                    <div
-                      class="d-flex flex-column    border border-dark rounded   "
-                      style={{ height: "280px" }}
-                    >
-                      <div class="    text-center  mx-auto   ">
-                        <h5 class="  mt-5">Upload Passport Size Image</h5>
-                        
+import React from "react";
 
-                        <button type="submit" class="btn btn-primary">Choose Picture</button>
-                      </div>
+const AddPatients = () => {
+  return (
+    <div>
+      <div className="container-fluid ">
+        <div className="row justify-content-center align-items-center ">
+          <div className="col-lg-6   d-flex   align-items-center gap-3 mt-5   ">
+            <div class="col-lg-6   ">
+              <div class="form-group p-2">
+                <div class=" ">
+                  <div
+                    class="d-flex flex-column    border border-dark rounded   "
+                    style={{ height: "280px" }}
+                  >
+                    <div class="    text-center  mx-auto   ">
+                      <h5 class="  mt-5">Upload Passport Size Image</h5>
+
+                      <button type="submit" class="btn btn-primary">
+                        Choose Picture
+                      </button>
                     </div>
                   </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
+              <div class="form-group p-2 col-lg-6 ">
+                <label>Select Division</label>
+                <select class="form-control select2" style={{ width: "100%" }}>
+                  <option selected="selected">Alabama</option>
+                  <option>Alaska</option>
+                  {/* <option disabled="disabled">California (disabled)</option> */}
+                  <option>Delaware</option>
+                  <option>Tennessee</option>
+                  <option>Texas</option>
+                  <option>Washington</option>
+                </select>
+              </div>
+              <div class="form-group p-2 col-lg-6">
+                <label>Select District</label>
+                <select class="form-control select2" style={{ width: "100%" }}>
+                  <option selected="selected">Alabama</option>
+                  <option>Alaska</option>
+                  {/* <option disabled="disabled">California (disabled)</option> */}
+                  <option>Delaware</option>
+                  <option>Tennessee</option>
+                  <option>Texas</option>
+                  <option>Washington</option>
+                </select>
+              </div>
+              <div class="form-group p-2 col-lg-6">
+                <label>Select Thana </label>
+                <select class="form-control select2" style={{ width: "100%" }}>
+                  <option selected="selected">Alabama</option>
+                  <option>Alaska</option>
+                  {/* <option disabled="disabled">California (disabled)</option> */}
+                  <option>Delaware</option>
+                  <option>Tennessee</option>
+                  <option>Texas</option>
+                  <option>Washington</option>
+                </select>
+              </div>
             </div>
           </div>
-
-          <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
-            <div class="form-group p-2 col-lg-6 "  >
-              <label>Select Division</label>
-              <select class="form-control select2" style={{ width: "100%" }}>
-                <option selected="selected">Alabama</option>
-                <option>Alaska</option>
-                {/* <option disabled="disabled">California (disabled)</option> */}
-                <option>Delaware</option>
-                <option>Tennessee</option>
-                <option>Texas</option>
-                <option>Washington</option>
-              </select>
-            </div>
-            <div class="form-group p-2 col-lg-6" >
-              <label>Select District</label>
-              <select class="form-control select2" style={{ width: "100%" }}>
-                <option selected="selected">Alabama</option>
-                <option>Alaska</option>
-                {/* <option disabled="disabled">California (disabled)</option> */}
-                <option>Delaware</option>
-                <option>Tennessee</option>
-                <option>Texas</option>
-                <option>Washington</option>
-              </select>
-            </div>
-            <div class="form-group p-2 col-lg-6">
-              <label>Select Thana </label>
-              <select class="form-control select2" style={{ width: "100%" }}>
-                <option selected="selected">Alabama</option>
-                <option>Alaska</option>
-                {/* <option disabled="disabled">California (disabled)</option> */}
-                <option>Delaware</option>
-                <option>Tennessee</option>
-                <option>Texas</option>
-                <option>Washington</option>
-              </select>
-            </div>
-          </div>
-
-
-          
         </div>
-        
-      </div>
 
-      <div className="row d-flex justify-content-center align-items-center">
-      <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
-            <div class="form-group p-2 col-lg-6 "  >
-              <label>Patients  Name</label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="Patients Name"/>
-                
+        <div className="row d-flex justify-content-center align-items-center">
+          <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
+            <div class="form-group p-2 col-lg-6 ">
+              <label>Patients Name</label>
+              <input
+                class="form-control select2"
+                style={{ width: "100%" }}
+                placeholder="Patients Name"
+              />
             </div>
-            <div class="form-group p-2 col-lg-6 "  >
+            <div class="form-group p-2 col-lg-6 ">
               <label>Patients Attendent Name</label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="Patients Attendent Name"/>
-                
+              <input
+                class="form-control select2"
+                style={{ width: "100%" }}
+                placeholder="Patients Attendent Name"
+              />
             </div>
-            <div class="form-group p-2 col-lg-6 "  >
-              <label>Patients  Mobile Number </label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="Patients  Mobile "/>
-                
+            <div class="form-group p-2 col-lg-6 ">
+              <label>Patients Mobile Number </label>
+              <input
+                class="form-control select2"
+                style={{ width: "100%" }}
+                placeholder="Patients  Mobile "
+              />
             </div>
             {/* <div class="form-group p-2 col-lg-6 "  >
               <label>Institution Name</label>
@@ -102,66 +108,78 @@
                 
             </div> */}
           </div>
-      </div>
-      <div className="row d-flex justify-content-center align-items-center">
-      <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
-            <div class="form-group p-2 col-lg-6 "  >
+        </div>
+        <div className="row d-flex justify-content-center align-items-center">
+          <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
+            <div class="form-group p-2 col-lg-6 ">
               <label>Age</label>
-              <input class="form-control select2"   placeholder="Patients  Age "/>
-                
+              <input
+                class="form-control select2"
+                placeholder="Patients  Age "
+              />
             </div>
-             
-            
-            <div class="form-group p-2 col-lg-6  "  >
+
+            <div class="form-group p-2 col-lg-6  ">
               <label> Patients Attendent Moile No</label>
-              <input class="form-control select2"   placeholder="Patients Attendent Moile No"/>
-                
+              <input
+                class="form-control select2"
+                placeholder="Patients Attendent Moile No"
+              />
             </div>
-             
-             
+            <div class="form-group p-2 col-lg-6  ">
+              <label>Add Patients Diagnosis</label>
+              <button className=" form-control btn btn-primary">Add</button>
+            </div>
           </div>
-      </div>
-            <div className="container">
-              <div className="row d-flex justify-content-center align-items-center">
-      <div class="col-lg-6 d-flex justify-content-between align-items-center p-2   ">
-            <div class="form-group p-2 col-lg-6 "  >
-              <label>  NID</label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="NID"/>
-                
-            </div>
-            <div class="form-group p-2 col-lg-6 "  >
-              <label>  Address</label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="Address"/>
-                
-            </div>
-            <div class="form-group p-2 col-lg-6 "  >
-              <label> Gender</label>
-              <input class="form-control select2" style={{ width: "100%" }} placeholder="Gender"/>
-                
-            </div>
-            
-             
-            {/* <div class="form-group p-2 col-lg-6 "  >
+        </div>
+        <div className="container">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div class="col-lg-6 d-flex justify-content-between align-items-center    ">
+              <div class="form-group p-2 col-lg-6 ">
+                <label> NID</label>
+                <input
+                  class="form-control select2"
+                  style={{ width: "100%" }}
+                  placeholder="NID"
+                />
+              </div>
+              <div class="form-group p-2 col-lg-6 ">
+                <label> Address</label>
+                <input
+                  class="form-control select2"
+                  style={{ width: "100%" }}
+                  placeholder="Address"
+                />
+              </div>
+              <div class="form-group p-2 col-lg-6 ">
+                <label> Gender</label>
+                <input
+                  class="form-control select2"
+                  style={{ width: "100%" }}
+                  placeholder="Gender"
+                />
+              </div>
+
+              {/* <div class="form-group p-2 col-lg-6 "  >
               <label>Mobile</label>
               <input class="form-control select2" style={{ width: "100%" }} placeholder="Mobile"/>
                 
             </div> */}
-          </div>
-         
-
-      </div>
-   
             </div>
-
-           <div className="container ">
-           <div className="row justify-content-end px-5
-">
-    <button className="btn btn-primary mb-3">Save</button>
-    </div>
-           </div>
-    </div>
+          </div>
         </div>
-    );
- };
- 
- export default AddPatients;
+
+        <div className="container ">
+          <div
+            className="row justify-content-end px-5
+"
+          >
+            <button className="btn btn-primary mb-3">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddPatients;
