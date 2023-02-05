f5839a1b93def2ef1f47b5827f185b31172145f4
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-05 12:49:54 +0600

    services responcive

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -31,21 +31,21 @@ const Aside = () => {
             </div>
           </div> */}
 
-          <div class="form-inline">
+          {/* <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-              {/* <input
+              <input
                 class="form-control form-control-sidebar"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
-              /> */}
+              />
               <div class="input-group-append">
-                {/* <button class="btn btn-sidebar">
+                <button class="btn btn-sidebar">
                   <i class="fas fa-search fa-fw"></i>
-                </button> */}
+                </button>
               </div>
             </div>
-          </div>
+          </div> */}
 
           <nav class="mt-2">
             <ul
