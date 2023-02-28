705e42e24dc691b9bfa6f1390e346c56d3b3b454
Sadnan-2019 <sadnan.wow@gmail.com>
2023-02-28 18:03:33 +0600

    services responcive

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useRef, useState } from 'react';
+import { useForm, useWatch } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
     // const navigate = useNavigate()
+// const [eamil ,setEmail] = useState("")
+// const [name ,setName] = useState("")
+// const [password ,setPassword] = useState("")
+const [email, setEmail] = useState("");
+const [name, setName] = useState("");
+// const [password, setPassword] = useState("");
+const [confirmpassword, setConfirmpassword] = useState("");
+const [error, setError] = useState("");
+// const navigate = useNavigate()    
+
+
+
     const {
       register,
       formState: { errors },
-      handleSubmit,
+      handleSubmit,watch
     } = useForm();
+    const password = useRef({});
+    password.current = watch("password", "");
     const onSubmit = (data) => console.log(data);
     return (
         <div>
@@ -60,6 +74,47 @@ const Register = () => {
                   </label>
                 </div>
               </div>
+
+              {/* name  */}
+              <div class=" input-group  ">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Name"
+                  name="name"
+                  {...register("name", {
+                    required: {
+                      value: "true",
+                      message: " name field is requried",
+                    },
+                    // pattern: {
+                    //   value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    //   message: "Provide a valid email address",
+                    // },
+                  })}
+                />
+
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-input-text"></span>  
+                  </div>
+                </div>
+              </div>
+              <div className=" text-danger">
+                <div className="col-lg-12 ">
+                  <label className="label">
+                    {errors.name?.type === "required" && (
+                      <span>{errors.name.message}</span>
+                    )}
+                    {/* {errors.name?.type === "pattern" && (
+                      <span>{errors.name.message}</span>
+                    )} */}
+                  </label>
+                </div>
+              </div>
+
+
+              {/* password  */}
               <div class=" input-group  ">
                 <input
                   type="password"
@@ -97,24 +152,32 @@ const Register = () => {
                 </div>
               </div>
 
-
+                        {/* confirm password  */}
               <div class=" input-group  ">
-                <input
-                  type="Confirm Password"
+                {/* <input
+                  type="password"
                   class="form-control"
                   placeholder="Confirm Password"
-                  name="password"
-                  {...register("password", {
+                  name="confirm-password"
+                  {...register("confirm-password", {
                     required: {
                       value: "true",
-                      message: " password field is requried",
-                    },
+                      message: "confirm-password field is requried",
+                  },
                     minLength: {
                       value: 4,
-                      message: "Password should be atleast 4 ", // JS only: <p>error message</p> TS only support string
+                      message: "confirm-password should be atleast 4 ", // JS only: <p>error message</p> TS only support string
                     },
                   })}
-                />
+                /> */}
+                  <input
+        name="password_repeat"
+        type="password"
+        ref={register({
+          validate: value =>
+            value === password.current || "The passwords do not match"
+        })}
+      />
 
                 <div class="input-group-append">
                   <div class="input-group-text">
@@ -125,11 +188,14 @@ const Register = () => {
               <div className=" text-danger">
                 <div className="col-lg-12 ">
                   <label className="label">
-                    {errors.password?.type === "required" && (
-                      <span>{errors.password.message}</span>
+                    {errors.confirm-password?.type === "required" && (
+                      <span>{errors.confirm-password.message}</span>
                     )}
-                    {errors.password?.type === "minLength" && (
-                      <span>{errors.password.message}</span>
+                    {errors.confirm-password?.type === "minLength" && (
+                      <span>{errors.confirm-password.message}</span>
+                    )}
+                    {errors.password === confirmpassword?.type === "" && (
+                      <span>{errors.confirm-password.message}</span>
                     )}
                   </label>
                 </div>
@@ -164,7 +230,7 @@ const Register = () => {
             </p> */}
             <p class="mb-0">
               <Link to="/login" class="text-center">
-Already User Plz Login              </Link>
+Already have an Account              </Link>
             </p>
           </div>
         </div>
